5feb62de34c44857df0c77f04ed739ab75477d76
Vojtech Parkan <voparkan@c3r2s1.42prague.com>
2025-03-07 16:30:02 +0100

    WrongAnimal implemented with WrongCat

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -5,18 +5,26 @@
 #include "Animal.h"
 #include "Cat.h"
 #include "Dog.h"
+#include "WrongAnimal.h"
+#include "WrongCat.h"
 
 int main()
 {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+    const WrongAnimal* w = new WrongAnimal();
+    const WrongAnimal* wc = new WrongCat();
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     i->makeSound(); //will output the cat sound!
     j->makeSound();
+    w->makeSound();
+    wc->makeSound();
     meta->makeSound();
 
+    delete w;
+    delete wc;
     delete j;
     delete i;
     delete meta;
