11e753bedd330db81bf976c000d94ff0f5ea9c8c
Vojtěch Parkán <czarte@gmail.com>
2025-03-08 17:25:54 +0100

    new lines in cpp, change Brain constructor and destructor message

--- CPP04/ex02/Brain.cpp
+++ CPP04/ex02/Brain.cpp
@@ -8,9 +8,9 @@ Brain::Brain() {
   for (int i = 0; i < 100; i++) {
     this->ideas[i] = "";
   }
-  std::cout << "Brain Constructor" << std::endl;
+  std::cout << "Brain::Brain()" << std::endl;
 }
 
 Brain::~Brain() {
-  std::cout << "Brain Destructor" << std::endl;
+  std::cout << "Brain::~Brain()" << std::endl;
 }
